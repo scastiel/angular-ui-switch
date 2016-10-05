0f9a4604267e1426095b8972713e6cdaa80a984d
Sébastien Castiel <sebastien@castiel.me>
2016-10-05 10:04:54 +0200

    make code a lot more modern

--- Makefile
+++ Makefile
@@ -8,7 +8,9 @@ publish: npm bower
 
 js:
 	@echo "Minifying javascript ..."
-	@$(NPM)/uglifyjs angular-ui-switch.js --compress --mangle --comments > angular-ui-switch.min.js
+	@$(NPM)/babel angular-ui-switch.js > angular-ui-switch.babel.js
+	@$(NPM)/uglifyjs angular-ui-switch.babel.js --compress --mangle --comments > angular-ui-switch.min.js
+	@rm angular-ui-switch.babel.js
 
 css:
 	@echo "Minifying css ..."
--- angular-ui-switch.js
+++ angular-ui-switch.js
@@ -1,28 +1,33 @@
-angular.module('uiSwitch', [])
-
-.directive('switch', function(){
-  return {
-    restrict: 'AE'
-  , replace: true
-  , transclude: true
-  , template: function(element, attrs) {
-      var html = '';
-      html += '<span';
-      html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
-      html +=   attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"') : '';
-      html +=   ' ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + ' }"';
-      html +=   '>';
-      html +=   '<small></small>';
-      html +=   '<input type="checkbox"';
-      html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
-      html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
-      html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
-      html +=     ' style="display:none" />';
-      html +=     '<span class="switch-text">'; /*adding new container for switch text*/
-      html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; /*switch text on value set by user in directive html markup*/
-      html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  /*switch text off value set by user in directive html markup*/
-      html += '</span>';
-      return html;
-    }
-  }
+angular.module('uiSwitch', []).directive('switch', function () {
+    return {
+        restrict: 'AE',
+        replace: true,
+        transclude: true,
+        scope: {
+            ngDisabled: '=',
+            ngModel: '=',
+            classes: '=',
+            on: '=',
+            off: '=',
+            id: '=',
+            name: '='
+        },
+        template: `
+            <span class="switch {{classes}}"
+                  ng-click="activated = !activated"
+                  ng-class="{ checked: ngModel, disabled: ngDisabled }">
+                <small></small>
+                <input id="{{id}}"
+                       name="{{name}}"
+                       type="checkbox"
+                       style="display: none"
+                       ng-disabled="ngDisabled"
+                       ng-model="ngModel">
+                <span class="switch-text">
+                    <span class="on" ng-if="on" ng-bind="on"></span>
+                    <span class="off" ng-if="off" ng-bind="off"></span>
+                </span>
+            </span>
+        `
+    };
 });
